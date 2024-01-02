c548987f1b32566474125a6b9691e148e34f0473
Brenna Pacheco <brennapacheco@hotmail.com>
2024-01-02 20:23:34 +0000

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Brenna Pacheco on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
